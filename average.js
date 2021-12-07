6c69bfa8eb8f18fd90e7f20a72a94a889f70cf80
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2021-12-07 18:26:33 +0530

    Check for CoV and Test Case

--- average.js
+++ average.js
@@ -1,7 +1,19 @@
 
-function average(numbers) {
+function average(numbers, threshold = 0.25) {
   const filtered = numbers.filter(number=>!isNaN(number));
-  return filtered.reduce((p, c)=> p + c, 0) / filtered.length;
+  const mean = filtered.reduce((p, c)=> p + c, 0) / filtered.length;
+  const difference = filtered.map((n, i, a) => n - (a[i-1] || 0));
+
+  const d_mean = difference.reduce((p, c)=> p + c, 0) / difference.length;
+  const d_variance = difference.reduce((p, c)=> p + Math.pow((c - d_mean), 2), 0) / difference.length;
+  const d_cov = Math.sqrt(d_variance) / d_mean;
+
+  if (d_cov > threshold) {
+    return NaN; 
+  } else {
+    return mean;
+  } 
+
 }
 
 module.exports = {average};
