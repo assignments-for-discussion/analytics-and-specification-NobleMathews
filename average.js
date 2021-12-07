a28d0e9339dbd98ce67893b892a20448154baa9c
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2021-12-07 18:35:57 +0530

    Lint Fix

--- average.js
+++ average.js
@@ -1,19 +1,19 @@
 
 function average(numbers, threshold = 0.25) {
-  const filtered = numbers.filter(number=>!isNaN(number));
+  const filtered = numbers.filter((number)=>!isNaN(number));
   const mean = filtered.reduce((p, c)=> p + c, 0) / filtered.length;
   const difference = filtered.map((n, i, a) => n - (a[i-1] || 0));
 
-  const d_mean = difference.reduce((p, c)=> p + c, 0) / difference.length;
-  const d_variance = difference.reduce((p, c)=> p + Math.pow((c - d_mean), 2), 0) / difference.length;
-  const d_cov = Math.sqrt(d_variance) / d_mean;
+  const dMean = difference.reduce((p, c)=> p + c, 0) / difference.length;
+  const varReducer = (p, c)=>p + Math.pow((c - dMean), 2);
+  const dVariance = difference.reduce(varReducer, 0) / difference.length;
+  const dCov = Math.sqrt(dVariance) / dMean;
 
-  if (d_cov > threshold) {
-    return NaN; 
+  if (dCov > threshold) {
+    return NaN;
   } else {
     return mean;
-  } 
-
+  }
 }
 
 module.exports = {average};
